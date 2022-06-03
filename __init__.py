4d512c9a270b2a7e336d5bfa2d37b4ead14092be
Melet Chirino Caicedo <melet.chirino@astc-design.com>
2022-06-03 16:13:56 +0200

    [UPDATE] Cefas app interfaces done #28

--- __init__.py
+++ __init__.py
@@ -1,22 +1,60 @@
 import jwt
 import time
+import datetime
 
-if __name__ == "__main__":
-    secret = 'my_secret'
-    payload_ = {
-            'user': 'Melet',
-            'email': 'melet.chirino@gmail.com',
-            'cedula': 1047478102,
-            'expiration_date': time.time() + 2*60,
-            }
-    token = jwt.encode(
-            payload = payload_,
-            key = secret
-            )
-    print(f"token = {token}")
-    decode_info = jwt.decode(token, key = secret, algorithms = ['HS256', ])
-    print(f"info = {decode_info}")
-    if decode_info['expiration_date'] < time.time():
-        print("expired")
+# local apps
+import apps.Carmeil
+
+def validate_token(token):
+    secret = os.getenv('SECRET_CEFAS')
+    try:
+        decoded_data = jwt.decode(token_, key=secret, algorithms=['HS256',])
+        if not is_expired(decoded_data['date']):
+            raise Exception('Expired link')
+        return decoded_data
+    except Exception as e:
+        return e
+
+def is_expired(exp_date):
+    exp_date = decoded_data['date'] + datetime.timedelta(days=3)
+    today = datetime.date.today()
+    if today < exp_date:
+        # if today is earlier than exp_date
+        return False
     else:
-        print("well done")
+        return True
+
+def generate_url(data):
+    data['date'] = datetime.date.today()
+    token = jwt.encode(
+            payload = data,
+            key = os.getenv('SECRET_CEFAS')
+            )
+    server = 'localhost:8000'
+    url = F'{server}/validation/?token={token}'
+    return url, token
+
+def new_user_request(data):
+    '''
+    In data['content'] you can add your message and type [url]
+    instead of url with token that will be then added
+    '''
+    if not(data.get('email') and data.get('header')
+            and data.get('subject') and data.get('content')):
+        raise Exception('Data must have email, header, subject, and content')
+    url, token = generate_token(data)
+    new_user_email = data['email']
+    header = data['header']
+    subject = data['subject']
+    content = data['content']
+    content = content.replace('[url]', url)
+    try:
+        Carmeil.send_mail(
+                receiver = new_user_email,
+                header = header,
+                subject = subject,
+                content = content
+                )
+        return 'OK'
+    except Exception as e:
+        return e
--- views.py
+++ views.py
@@ -3,33 +3,20 @@ from django.http import JsonResponse
 #python modules
 import os
 import jwt
+from apps.Cefas import validate_token
 
 def validation(request):
     '''
     http://localhost:8000/validation/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ZHNkODkwIiwibmFtZSI6IkogQmFsdmluIiwiaWF0IjoxNTEzOTAyMiwid2lzaW4iOiJZYW5kZWwifQ.rr_uc-ebLpabO353a44PiPETwCFzb3X4O-3y9wGi57wj
     '''
+
     print(F'data = {request.GET}')
     token_ = request.GET['token']
     print(F'token = {token_}')
-    secret = os.getenv('SECRET_CEFAS')
-    decoded_data = jwt.decode(token_, key=secret, algorithms=['HS256',])
-    '''
-    Ahora debes validar la fecha de expiracion, si el link es valido redirige
-    al usuario a un form donde de verdad se van a guardar los datos del usuario.
-    '''
-    return JsonResponse(decoded_data)
+    try:
+        decoded_data = validate_token(token)
 
-def generate_url(data):
-    token = jwt.encode(
-            payload = data,
-            key = os.getenv('SECRET_CEFAS')
-            )
-    server = 'localhost:8000'
-    url = F'{server}/validation/?token={token}'
-    return url
-
-def new_user_request(request):
-    # esto deberia ser un formView en el que le pases datos de payload y luego
-    # los mandas por email con carmeil.
-    pass
-
+        #this is an interface of admin2
+        new_user_form(data)
+    except Exception as e:
+        return e
